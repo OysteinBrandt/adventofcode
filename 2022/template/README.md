c848b750fe9fd464384b08a7ed4c08055d130a51
Øystein Brandt <oystein.brandt@gmail.com>
2022-12-04 01:48:10 +0100

    Add solution template folder

---
+++ 2022/template/README.md
@@ -0,0 +1,2 @@
+# <NAME_OF_TASK>
+<url_to_task>
---
+++ 2022/template/main.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+def read_input():
+  # NOTE: It appears that the input might change during the event.
+  #       Use website to get latest data before delivering answer.
+  input_file_path = Path(__file__).parent / 'input.txt'
+  # input_file_path = Path(__file__).parent / 'example_input.txt'
+  with open(input_file_path, 'r') as file:
+    return file.read().splitlines()
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
